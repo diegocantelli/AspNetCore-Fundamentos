30134f9180a964c7479be5a14be6008b913ba234
Diego Cantelli <diegocantelli19@gmail.com>
2021-05-08 14:36:09 -0300

    initial commit

---
+++ DesenvolvedorIoDemoMVC/DesenvolvedorIoDemoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesenvolvedorIoDemoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
